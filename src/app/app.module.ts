03146667ccfbe3d48d3616c7ea9f9473fcb72206
rafi007akhtar <alimdrafi@gmail.com>
2019-11-02 17:30:12 +0530

    Added a service to display the mock data, instead of doing it on the same component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HeroesComponent } from './heroes/heroes.component';
 
 import { FormsModule } from '@angular/forms';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroService } from './hero.service';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,7 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
     AppRoutingModule,
     FormsModule
   ],
-  providers: [],
+  providers: [HeroService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,26 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { Hero } from './hero';
-import { HEROES } from '../mock-heroes';
-import { NONE_TYPE } from '@angular/compiler/src/output/output_ast';
+// import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  styleUrls: ['./heroes.component.css'],
+  providers: [HeroService]
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
-
   hero: Hero = {id: 1, name: 'Windstorm'};
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
+
+  constructor(
+    @Inject(HeroService) private heroService: HeroService
+  ) { }
+
+  ngOnInit() {
+    this.getHeroes();
+  }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
 
-  ngOnInit() {
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
   }
 
 }
