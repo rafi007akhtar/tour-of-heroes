26471711d646b5bca8731c26377d47e50ab99553
rafi007akhtar <alimdrafi@gmail.com>
2019-11-07 02:32:22 +0530

    Successfully implemented the dashboard route.
    - the HeroesDetail component now works as a routeLink, instead of by a (click) directive
    - the HeroService sends the Observable as the HEROES array, to be received by HeroesDetail Component and display accordingly

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -17,9 +17,20 @@ export class HeroDetailComponent implements OnInit {
     private heroService: HeroService
   ) { }
 
-  @Input() hero: Hero;
+  hero: Hero;
 
   ngOnInit() {
+    this.getHero();
+  }
+
+  getHero(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    console.log(id);
+    this.heroService.getHero(id)
+      .subscribe(hero => {
+        this.hero = hero;
+        console.log(hero.name);
+    });
   }
 
 }
